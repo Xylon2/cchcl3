8069a5ac30cf96a03da5bf0ce5f56e48a9f93811
Joseph Graham <joseph@xylon.me.uk>
2022-05-18 20:02:42 +0100

    Add queries to create and list choices

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -7,3 +7,14 @@ values (:question_text)
 -- :name get-questions :? :*
 -- :doc retrieves all questions
 select * from question
+
+-- :name create-choice! :! :n
+-- :doc creates a choice
+insert into choice
+(question, choice_text)
+values (:question, :choice_text)
+
+-- :name get-choices :? :*
+-- :doc retrieves all choices for a question
+select * from choice
+where question = :question
